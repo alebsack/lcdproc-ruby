51706fe820bcb17170d562639dd28976bd7cce84
Adam Lebsack <adam.lebsack@sophos.com>
2013-04-14 12:32:16 +0200

    rearrange some stuff

---
+++ lib/lcdproc.rb
@@ -0,0 +1,14 @@
+require 'core_extensions/string'
+require 'core_extensions/array'
+
+module LCDProc
+  autoload :Screen, "lcdproc/screen"
+  autoload :Client, "lcdproc/client"
+  autoload :EMClient, "lcdproc/em_client"
+  autoload :Command, "lcdproc/command"
+  autoload :Response, "lcdproc/response"
+  autoload :Widget, "lcdproc/widget"
+  autoload :Menu, "lcdproc/menu"
+  autoload :Devices, "lcdproc/devices"
+  
+end
